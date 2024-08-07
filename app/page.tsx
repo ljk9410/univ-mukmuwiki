f772d0947c82e570b6e972d0c2a35dd83113296e
이정규 <dkssud6115@gmail.com>
2024-08-07 15:23:12 +0900

    PWA 설정

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">
-			<p>먹무위키 테스트</p>
+			<p>먹무위키 테스트 PWA 설정</p>
 		</main>
 	);
 }
