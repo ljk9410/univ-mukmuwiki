2f382d2f48182ef347ce9ccf8e5fc94c5eb17f49
이정규 <dkssud6115@gmail.com>
2024-08-15 17:04:17 +0900

    사이드바 UI

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,6 +4,9 @@ import SidebarButton from './SidebarButton';
 import PostView from './PostView';
 import PostForm from './PostForm';
 import { Post } from '../lib/types';
+import Image from 'next/image';
+import { ListBulletIcon, MapIcon } from '@heroicons/react/24/solid';
+import classNames from 'classnames';
 
 type Props = {
 	isOpen: boolean;
@@ -22,12 +25,38 @@ const Sidebar = ({ isOpen, existingPost, setIsOpen }: Props) => {
 	return (
 		<div className="z-10 relative flex">
 			<header
-				className={`z-50 fixed w-16 left-0 top-0 h-full flex bg-slate-50 ${
+				className={`z-50 fixed w-16 left-0 top-0 h-full flex flex-col items-center bg-slate-50 ${
 					!isOpen ? 'shadow-right' : 'border-r-[1px] border-t-slate-950'
-				}`}
+				} border-r-[1px] border-gray-300`}
 			>
-				<p>로고박스</p>
+				<h1>
+					<button className="py-3 border-b-[1px] border-gray-300">
+						<Image
+							src={'/cau_logo.png'}
+							width={100}
+							height={100}
+							alt="university logo"
+						/>
+					</button>
+				</h1>
+				<nav className="w-full">
+					<ol>
+						<li>
+							<button className={styles.navButton}>
+								<MapIcon className={styles.navButtonIcon} />
+								<p className={styles.navButtonText}>지도</p>
+							</button>
+						</li>
+						<li>
+							<button className={styles.navButton}>
+								<ListBulletIcon className={styles.navButtonIcon} />
+								<p className={styles.navButtonText}>리스트</p>
+							</button>
+						</li>
+					</ol>
+				</nav>
 			</header>
+
 			<Transition
 				show={isOpen}
 				enter="transition-transform duration-500"
@@ -53,6 +82,16 @@ const Sidebar = ({ isOpen, existingPost, setIsOpen }: Props) => {
 			</Transition>
 		</div>
 	);
+};
+
+const styles = {
+	navButton: classNames(
+		'w-full h-[66px] flex flex-col justify-center items-center hover:bg-[#0675F4] group'
+	),
+	navButtonIcon: classNames(
+		'w-[20px] text-[#3C3C3C] mb-[1px] group-hover:text-white'
+	),
+	navButtonText: classNames('text-[13px] group-hover:text-white'),
 };
 
 export default Sidebar;
