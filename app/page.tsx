3fab288747ba9d3b61302538b433065ed43f9be7
이정규 <dkssud6115@gmail.com>
2024-08-21 14:39:13 +0900

    페이지 갱신

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { getPostData } from './api/postAPI';
 import MainView from './components/MainView';
 
+export const revalidate = 10; // 10초마다 페이지를 갱신
+
 export default async function Home() {
 	const postDataList = await getPostData('post'); // TODO: try catch 걸기
-	console.log(postDataList);
 
 	return (
 		<main className="flex min-h-screen w-full h-screen flex-col items-center justify-between">
